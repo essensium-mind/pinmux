0cd6ed273e54ceb8e7ae64895a2b3c0a18ce38a8
Thomas Perale <thomas.perale@mind.be>
2024-05-02 14:23:25 +0200

    devicetree: Pin direction small fix
    Temporary solution to improve the direction depending on the pin
    function.

--- src/components/dts/DeviceTree.js
+++ src/components/dts/DeviceTree.js
@@ -36,7 +36,14 @@ export function DeviceTreeOutput() {
           out += `\t${proto}${bus}_pins: pinmux_${proto}${bus}_pins {\n\t\tpinctrl-single,pins = <\n`;
 
           busses[bus].forEach(({address, mode, func, number }) => {
-            out += `\t\t\tAM33XX_IOPAD(${address}, PIN_INPUT_PULLUP | MUX_MODE${mode}); /* ${proto.toUpperCase()}${bus}_${func ? func.toUpperCase() : number} */\n`;
+            const direction = func === 'SCLK' ?
+              'PIN_OUTPUT_PULLUP'
+                : func === 'TXD' ? 
+              'PIN_OUTPUT_PULLDOWN'
+                : 
+              'PIN_INPUT_PULLUP'
+
+            out += `\t\t\tAM33XX_IOPAD(${address}, ${direction} | MUX_MODE${mode}); /* ${proto.toUpperCase()}${bus}_${func ? func.toUpperCase() : number} */\n`;
           })
 
           out += `\t\t>;\n\t};\n\n`;
