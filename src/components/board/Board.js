f890d6eb5c21c80c0db4fef30dc4351c1967e7a6
Thomas Perale <thomas.perale@mind.be>
2024-05-07 23:06:58 +0200

    board: pixel perfect tracking of the header overlay
    Add support for dynamic board image resizing with support for the header
    overlay to perfectly match the one on the board image.

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useSelectedPinContext } from '../../contexts/pins.js';
 import { useSelectedHeaderContext, SelectedHeaderProvider } from '../../contexts/header.js';
 import { useResizeObserver, useTableSizeObserver } from '../../hooks/resize.js';
@@ -51,7 +51,7 @@ const Legend = ({ align, id, desc, shown }) => (
   </td>
 );
 
-function Pin({ pin, shown }) {
+function Pin({ offset, innerSize, borderSize, pin, shown }) {
   const UnwrappedPin = ({ shown, id, justify }) => {
     const desc = (align) => (bone.pins[id] ? (
       <Legend shown={shown} id={id} align={align} desc={bone.pins[id]}/>) : (
@@ -60,7 +60,7 @@ function Pin({ pin, shown }) {
     return (
       <>
         { justify === "left" ? desc('right') : null }
-        <td className="female-pin"/>
+        <td style={{ height: innerSize + offset, width: innerSize, borderWidth: borderSize }} className="female-pin"/>
         { justify === "right" ? desc('left') : null }
       </>
     );
@@ -87,7 +87,7 @@ function Pin({ pin, shown }) {
   }
 }
 
-function Header({ header, imageSize }) {
+function Header({ ratio, pitch, header, imagePos }) {
   const { select, selectedHeader } = useSelectedHeaderContext()
   const shown = selectedHeader === header.name;
 
@@ -102,9 +102,38 @@ function Header({ header, imageSize }) {
     xOffset = 0;
   } else {
     xOffset = header.justify === 'vertical' ?
-      (imageSize.imageLeft + header.position.x) :
-      (imageSize.imageLeft + header.position.x - columnSize[0])
+      (imagePos.left + (ratio * header.position.x)) :
+      (imagePos.left + (ratio * header.position.x) - columnSize[0])
   }
+
+  // FIXME Only vertical header are supported so far.
+  const headerLength = header.contents.length;
+  const adjustedPitch = ratio * pitch;
+
+  // Because pixels are not precise enough to perfectly match with the header
+  // representation in the image. We need to add a pixel offset on a regular
+  // basis to compensate the mismatch in length.
+
+  // The following variable are computed based on a visual representation
+  // I found pleasing and does not follow any guidelines.
+  const _borderPixelGrow = 1; // How much bigger in pixel the border is relative to innerSize
+  const _pinSize = (Math.floor(adjustedPitch) - (2 * _borderPixelGrow)) / 3;
+  const innerSize = Math.floor(_pinSize) + Math.round((_pinSize - Math.floor(_pinSize)) * 3);
+  const borderSize = Math.floor(_pinSize + _borderPixelGrow);
+
+  // Based on the rounded header pixel height, compute how many pixels we have
+  // to compensate to match the representation of the header in the image.
+  const _headerRepresentationTotalHeight = innerSize + (2 * borderSize);
+  const missingPixels = Math.round(adjustedPitch * headerLength) - (_headerRepresentationTotalHeight * headerLength)
+
+  // Create an evenly distribution of the offsets.
+  const offsetIndex = (missingPixels < (headerLength / 2)) ?
+    Array.apply(null, Array(headerLength)).map((_, i) => {
+      return Number((i % Math.floor(headerLength / missingPixels)) === 0)
+    }) :
+    Array.apply(null, Array(headerLength)).map((_, i) => {
+      return Number((i % Math.floor(headerLength / (headerLength - missingPixels))) > 0)
+    });
 
   return (
     <div
@@ -116,8 +145,8 @@ function Header({ header, imageSize }) {
       <div 
         style={{
           position: 'absolute',
-          top: imageSize.imageTop + header.position.y - headerHeight - 18,
-          left: imageSize.imageLeft + header.position.x,
+          top: imagePos.top + (ratio * header.position.y) - headerHeight - 18,
+          left: imagePos.left + (ratio * header.position.x),
         }} 
         ref={headerRef}
         className={`pin-header-title pin-header-title-${shown ? 'selected' : 'hidden'}`}
@@ -127,14 +156,14 @@ function Header({ header, imageSize }) {
       <table
         style={{
           position: 'absolute',
-          top: imageSize.imageTop + header.position.y,
+          top: imagePos.top + (ratio * header.position.y),
           left: xOffset,
           zIndex: shown ? 10 : 5
         }}
       >
         <tbody ref={tableRef}>
           {header.contents.map((pin, index) => (
-            <Pin key={`pin-header-line-${header.name}-${index}`} shown={shown} pin={pin} />
+            <Pin offset={offsetIndex[index]} innerSize={innerSize} borderSize={borderSize} key={`pin-header-line-${header.name}-${index}`} shown={shown} pin={pin} />
           ))}
         </tbody>
       </table>
@@ -147,24 +176,37 @@ export function Board () {
   const imgRef = useRef(null);
   const { width: containerWidth, height: containerHeight } = useResizeObserver(containerRef);
   const { width: imgWidth, height: imgHeight } = useResizeObserver(imgRef);
+  const [ imgOriginalDimension, setImgOriginalDimension ] = useState({
+    originalWidth: undefined,
+    originalHeight: undefined,
+  });
+
+  const onImgLoad = ({ target }) => {
+    setImgOriginalDimension({
+      originalWidth: target.naturalWidth,
+      originalHeight: target.naturalHeigh
+    });
+  }
 
   // Offset relative to the container the image is wrapped in.
   // The image is centered inside that container.
-  const { imageTop, imageLeft } = { 
-    imageLeft: (containerWidth - imgWidth) / 2,
-    imageTop: (containerHeight - imgHeight) / 2,
+  const imagePos = { 
+    left: (containerWidth - imgWidth) / 2,
+    top: (containerHeight - imgHeight) / 2,
   }
 
   return (
     <SelectedHeaderProvider headerInit={bone.headers.length ? bone.headers[0].name : ''}>
       <div className="board-container" style={{ minWidth: containerWidth }} ref={containerRef}>
-        <img ref={imgRef} src={require(`../../assets/images/${bone.metadata.image}`)} alt={bone.metadata.name}/>
+        <img onLoad={onImgLoad} ref={imgRef} src={require(`../../assets/images/${bone.metadata.image}`)} alt={bone.metadata.name}/>
         <div className="pin-overlay">
           {bone.headers.map(header =>
             <Header 
-                imageSize={{ imageTop, imageLeft }}
-                key={`pin-header-${header.name}`} 
-                header={header}
+              ratio={(imgOriginalDimension.originalWidth && containerWidth) ? (imgWidth / imgOriginalDimension.originalWidth) : 1}
+              pitch={header.pitch}
+              imagePos={imagePos}
+              key={`pin-header-${header.name}`} 
+              header={header}
             />
           )}
         </div>
