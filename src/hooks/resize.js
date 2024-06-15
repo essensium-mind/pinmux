320fb12223884068f3c447baf36e0b8c12899d13
Thomas PERALE <perale.thomas@gmail.com>
2024-06-15 09:57:44 +0200

    fix(hooks/resize): recheck the reference size on useResizeObserver call
    On first page load the ResizeObserver might not be triggered to notify
    the first resize. This is a temporary fix.

--- src/hooks/resize.js
+++ src/hooks/resize.js
@@ -1,8 +1,24 @@
 import { useState, useEffect } from 'react';
 
 export function useResizeObserver (ref) {
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(ref.current ? ref.current.offsetWidth : 0);
+  const [height, setHeight] = useState(ref.current ? ref.current.offsetHeight : 0);
+
+  // FIXME Have to force setting the state outside of the observer to verify it
+  // on useResizeObserver call.
+  // On some case when changing the page the width & height state would be set
+  // to 0 but the ResizeObserver wouldn't trigger a recalc and the width &
+  // height would remain equals to 0.
+  // With this solution for now we can double check the value is updated
+  if (ref.current) {
+      if(ref.current.offsetWidth !== width) {
+        setWidth(ref.current.offsetWidth)
+      }
+
+      if(ref.current.offsetHeight !== height) {
+        setHeight(ref.current.offsetHeight)
+      }
+  }
 
   useEffect(() => {
     if (!ref.current) {
@@ -11,11 +27,11 @@ export function useResizeObserver (ref) {
 
     const resizeObserver = new ResizeObserver(() => {
       if(ref.current.offsetWidth !== width) {
-        setWidth(ref.current.offsetWidth); 
+        setWidth(ref.current.offsetWidth)
       }
 
       if(ref.current.offsetHeight !== height) {
-        setHeight(ref.current.offsetHeight); 
+        setHeight(ref.current.offsetHeight)
       }
     });
     
